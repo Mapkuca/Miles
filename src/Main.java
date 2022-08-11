0366a7dac9f56fbec90d93d82f0571d8c9dc37b7
Алена Кудрявцева <sunny_aga@mail.ru>
2022-08-11 16:09:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 15000;
+        int miles = ticketPrice / 20;
+
+        System.out.println("Вам начислено: " + miles + " миль.");
+    }
+}
